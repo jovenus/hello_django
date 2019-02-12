ad36a7ab968d7361c9bcb3e76838f327e79adcf1
jovenus <jovenus@outlook.kr>
2019-02-12 12:18:24 +0900

    히로쿠 업뎃2

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$4b3(4#gy&8mya9n1x2ez5hbxd+(sqc(t$-577feq*&xyrfhq3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
