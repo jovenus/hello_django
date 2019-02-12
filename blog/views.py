944fca6b375fc6a390a7bdffe78fe51dbd1d1a63
jovenus <jovenus@outlook.kr>
2019-02-12 11:01:45 +0900

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+def index(request):
+    return render(request, 'blog/index.html')
+    
+def hello_times(request, times):
+    message = "안녕하세요"*times
+    return HttpResponse(message)
