282ae1cece6287977cdb28b3489baff873eb7c66
jovenus <jovenus@outlook.kr>
2019-02-13 12:35:26 +0900

    네이버 실시간 검색어 뷰 샘플 구현

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
 from blog.views import index, hello_times
 from blog.views import articles_by_year
+from blog.views import naver_realtime_keywords
 
 from django.urls import register_converter
 from blog.converters import FourDigitYearConverter
@@ -24,4 +25,5 @@ urlpatterns = [
 
     path('hello_times/<int:times>/', hello_times),
     # re_path(r'blog/hello_times/(?P<times>\d+)/$', hello_times),
+    path('naver/실시간검색어/', naver_realtime_keywords),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
@@ -12,3 +14,11 @@ def articles_by_year(request, year):
 def hello_times(request, times):
     message = "안녕하세요"*times
     return HttpResponse(message)
+
+def naver_realtime_keywords(request):
+    res = requests.get("http://naver.com")
+    html = res.text
+    soup = BeautifulSoup(html, 'html.parser')
+    tag_list = soup.select('.PM_CL_realtimeKeyword_rolling .ah_k')
+    text = '\n'.join([tag.text for tag in tag_list])
+    return HttpResponse(text)
