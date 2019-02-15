5af704f22a4e460c6948c674ed158fe99326e476
Andy Cho <jovenus@outlook.kr>
2019-02-15 10:10:16 +0900

    /blog/ 주소로 이동하는 root 뷰 구현

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -1,9 +1,15 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponseRedirect
 
+def root(request):
+    return HttpResponseRedirect('/blog/')
+    # return HttpResponseRedirect('http://naver.com')
+    
 urlpatterns = [
 path('admin/', admin.site.urls),
 path('blog/', include('blog.urls')),
 path('shop/', include('shop.urls')),
+path('', root),
 ]
