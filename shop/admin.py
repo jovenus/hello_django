26569e31605cd0438ed0dc84ca1fc30b49bf2645
조병옥 <jovenus@outlook.kr>
2019-02-18 17:18:55 +0900

    shop/ item 이름 표시

--- shop/admin.py
+++ shop/admin.py
@@ -3,15 +3,18 @@ from .models import Shop, Item
 # Register your models here.
 @admin.register(Shop)
 class ShopAdmin(admin.ModelAdmin):
-    # list_display = ['shop', 'name', 'price']
-    # list_display_links = ['name']
+    list_display = ['name', 'address']
+    list_display_links = ['name']
     # list_filter = ['is_publish']
-    # search_fields = ['name']
+    search_fields = ['name']
 
 
-    def short_content(self, shop):
-        return post.content[:20]
+    # def short_content(self, shop):
+    #     return post.content[:20]
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'price', 'shop']
+    list_display_links = ['name']
+    # list_filter = ['is_publish']
+    search_fields = ['name']
