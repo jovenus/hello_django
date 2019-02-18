5ca45bb00dac60beb2ffeaecf080cff243d19fd6
Andy Cho <jovenus@outlook.kr>
2019-02-18 09:56:10 +0900

    blog.Comment 모델 생성 및 admin에 등록

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,9 @@ class Post(models.Model):
 
     # class Meta:                  #migration 대상에서 빼고 싶을 때 지정
     #     manaaged = False 
+
+class Comment (models.Model):
+    author_name = models.CharField(max_length=100)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
