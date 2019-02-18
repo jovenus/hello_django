87927d23e136c26a5181795e22f045f2a1546720
Andy Cho <jovenus@outlook.kr>
2019-02-18 10:31:03 +0900

    필수필드 tags를 blog.post에 추가

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    tags = models.TextField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # pass
