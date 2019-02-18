04a8187ee3d11916d01940661d565b0c7e278843
Andy Cho <jovenus@outlook.kr>
2019-02-18 11:19:29 +0900

    blog.Post 모델에 대한 admin 커스텀 설정 추가

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 from .models import Post, Comment
 # from .models import Comment
 
-admin.site.register(Post)
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'short_content', 'is_publish', 'tags', 'created_at']
+    list_display_links = ['title']
+    list_filter = ['is_publish']
+    search_fields = ['title']
 
-admin.site.register(Comment)
+
+    def short_content(selg, post):
+        return post.content[:20]
+
+        
+
+
+
+    # pass
+
+# admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, db_index=True)
     content = models.TextField()
     tags = models.TextField(max_length=20)
+    is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
     # pass
 
     # class Meta:                  #migration 대상에서 빼고 싶을 때 지정
