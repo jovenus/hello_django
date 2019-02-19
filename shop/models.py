92f5760b1c3a463c5ba5b03924a57f572b452202
Andy Cho <jovenus@outlook.kr>
2019-02-19 09:53:20 +0900

    shop model 수정(item 이름)

--- shop/models.py
+++ shop/models.py
@@ -24,3 +24,6 @@ class Item (models.Model):
     is_public = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
