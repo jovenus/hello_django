7ec9d5e94a5968cafe710245fd7c08c5642342f3
Andy Cho <jovenus@outlook.kr>
2019-02-19 09:31:32 +0900

    shop 이름 표시 (어드민 아니라 모델에서)

--- shop/models.py
+++ shop/models.py
@@ -9,6 +9,12 @@ class Shop (models.Model):
     address = models.CharField(max_length=50, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+
+
+
 
 class Item (models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
