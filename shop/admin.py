be353cb8a995998f77f9e005e728cb860c683ca5
Andy Cho <jovenus@outlook.kr>
2019-02-19 09:29:42 +0900

    수업 예제 풀이(수정)

--- shop/admin.py
+++ shop/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Shop, Item
 # Register your models here.
-@admin.register(Shop)
+@admin.register(Shop)                 # 장식자 (decorate 문법)
 class ShopAdmin(admin.ModelAdmin):
-    list_display = ['name', 'address']
+    list_display = ['name', 'address', 'created_at']
     list_display_links = ['name']
     # list_filter = ['is_publish']
     search_fields = ['name']
+
+    def __str__(self):
+        return self.name
 
 
     # def short_content(self, shop):
@@ -14,7 +17,7 @@ class ShopAdmin(admin.ModelAdmin):
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'shop']
+    list_display = ['name', 'price', 'is_public', 'created_at', 'shop']
     list_display_links = ['name']
     # list_filter = ['is_publish']
     search_fields = ['name']
--- shop/models.py
+++ shop/models.py
@@ -6,7 +6,9 @@ from django.db import models
 class Shop (models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(blank=True)
-    address = models.CharField(max_length=50)
+    address = models.CharField(max_length=50, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Item (models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
@@ -14,3 +16,5 @@ class Item (models.Model):
     desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
     is_public = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
